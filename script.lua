a38905010e4a984b5d47efadf0594106f87a676a
Hjgabri <106931409+Hjgabri@users.noreply.github.com>
2024-06-01 02:14:54 -0300

    script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,25 @@
-oi
+local player = game.Players.LocalPlayer
+local char = player.Character or player.CharacterAdded:Wait()
+local h = char:WaitForChild('Humanoid')
+local character = player.Character
+
+
+h.WalkSpeed = 50
+h.JumpPower = 50
+
+
+local function infinity(player)
+	if character then
+		local humanoid = character:FindFirstChildOfClass("Humanoid")
+		if humanoid then
+			humanoid.MaxHealth = math.huge
+			humanoid.Health = math.huge
+		end
+	end
+end
+
+game.Players.PlayerAdded:Connect(function(player)
+	player.CharacterAdded:Connect(function()
+		infinity(player)
+	end)
+end)
